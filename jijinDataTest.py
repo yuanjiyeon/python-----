7ac7adf6f2cecc5369fb87d94c4063064e321987
yuanjiyeon <64567305+yuanjiyeon@users.noreply.github.com>
2020-06-11 16:48:26 +0800

    Add files via upload

--- jijinDataTest.py
+++ jijinDataTest.py
@@ -1,59 +1,28 @@
 import requests
 import json
-def selectParam(jijin):
-      if jijin=='110022':
-            params = {
-                'callback': 'jQuery18305571026460309254_1591837732526',
-                'fundCode': '110022',
-                'pageIndex': '1',
-                'pageSize': '30',
-                'startDate' : '2020-04-01',
-                'endDate': '2020-06-10',
-                '_': '1591838177370'
-            }
-      elif jijin=='003095':
-          params = {
-              'callback': 'jQuery18303586407702383334_1591845298937',
-              'fundCode': '003095',
-              'pageIndex': '1',
-              'pageSize': '30',
-              'startDate': '2020-04-01',
-              'endDate': '2020-06-10',
-              '_': '1591845338804'
-          }
-      elif jijin=='161903':
-          params = {
-              'callback': 'jQuery1830816396249439191_1591846172940',
-              'fundCode': '161903',
-              'pageIndex': '1',
-              'pageSize': '30',
-              'startDate': '2020-04-01',
-              'endDate': '2020-06-10',
-              '_': '1591857578727'
-          }
-      else:
-          params = {
-              'callback': 'jQuery183040296283814599976_1591858757195',
-              'fundCode': '110011',
-              'pageIndex': '1',
-              'pageSize': '30',
-              'startDate': '2020-04-01',
-              'endDate': '2020-06-10',
-              '_': '1591858777973'
-          }
-      return params
-# 161903---110022---003095----110011
-params=selectParam('110022')
+def selectParam(jijin):#只要改变基金的代码就可以实现查询
+    params = {
+        'callback': 'jQuery18305571026460309254_1591837732526',#随机
+        'fundCode': '110022',#基金代码
+        'pageIndex': '1',#页数
+        'pageSize': '30',#查询天数
+        'startDate': '2020-04-01',#开始日期
+        'endDate': '2020-06-10',#结束日期
+        '_': '1591838177370'#随机
+    }
+    params['fundCode']=jijin
+    return params
+# 161903（百家）---110022（易方达消费）---003095（中欧医疗）----110011（易方达）
+
+params=selectParam(input("输入基金代码"))
 hearders={
         'User-Agent' :  'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36',
         'Referer'    :  'http://fundf10.eastmoney.com/jjjz_110022.html'
     }
 host='http://api.fund.eastmoney.com/f10/lsjz?'
 t=requests.get(host,headers=hearders,params=params)
-if params['fundCode']=='161903':
+if params['fundCode']=='161903 ':
     t=t.text[40:-1]
-elif params['fundCode']=='110011':
-    t=t.text[42:-1]
 else:
     t=t.text[41:-1]
 m=json.loads(t)
